8c7fa9a308acff9123b91461f960413bb4a0c329
acasaso <131499472+acasaso@users.noreply.github.com>
2023-05-03 23:58:57 -0500

    Trabajo en clase de javascript
    Seguimiento de clase sesión 9

---
+++ main.js
@@ -0,0 +1,12 @@
+//alert(" Prueba Javascript");
+var nombre = "Angelica";
+var edad = 26
+var estatura = 160
+
+var datos = document.getElementById("datos");
+
+datos.innerHTML = `
+    <h2> • Mi nombre es: ${nombre} </h2>
+    <h2> • Mi nombre es: ${edad} </h2>
+    <h2> • Mi nombre es: ${estatura} </h2>
+`;
